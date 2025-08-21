b4d261bfe2b22412fb5c482606c3588cf1e7fb5e
ajinkyaabhang <ajinkyaabhang@gmail.com>
2025-08-21 22:03:26 +0200

    langsmith integration

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -79,6 +79,17 @@ if user_input:
         st.text(user_input)
 
     CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    '''
+    ############## for Langsmith integration ###############
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
+    '''
+    
 
     # first add the message to message_history
     with st.chat_message('assistant'):
